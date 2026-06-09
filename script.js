02c9c220a1dd3159ff80a9f0da8ec8431f8f1a86
Sks <124144700+sks123git@users.noreply.github.com>
2026-06-09 23:00:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const byteSize = (str) => {
-  // write your code here
+  let bytes = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    const codePoint = str.codePointAt(i);
+
+    // If it's a surrogate pair (emoji or rare symbols), skip the next index
+    if (codePoint > 0xffff) {
+      i++;
+    }
+
+    if (codePoint <= 0x7f) {
+      bytes += 1; // ASCII
+    } else if (codePoint <= 0x7ff) {
+      bytes += 2;
+    } else if (codePoint <= 0xffff) {
+      bytes += 3;
+    } else {
+      bytes += 4;
+    }
+  }
+	
+  return bytes;
 };
 
 // Do not change the code below
